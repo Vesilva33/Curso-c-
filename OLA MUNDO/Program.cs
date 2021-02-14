9752db0141e108405a1715e56e39d516ecd443c9
Verusca silva <56118750+Vesilva33@users.noreply.github.com>
2021-02-13 21:27:13 -0300

    Curso de c#

---
+++ OLA MUNDO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OLA_MUNDO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome");
+            var nome =Console.ReadLine();
+            Console.WriteLine("Ola,  " + nome);
+            
+        }
+    }
+}
